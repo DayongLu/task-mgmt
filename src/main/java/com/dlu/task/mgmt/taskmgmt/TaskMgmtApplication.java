10f1131d820325c28e00ff64cf382fbb81b9bbaf
DayongLu <ludayong20@gmail.com>
2018-09-13 14:53:24 -0400

    Initial commit

---
+++ src/main/java/com/dlu/task/mgmt/taskmgmt/TaskMgmtApplication.java
@@ -0,0 +1,12 @@
+package com.dlu.task.mgmt.taskmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMgmtApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dlu/task/mgmt/taskmgmt/TaskMgmtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dlu.task.mgmt.taskmgmt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskMgmtApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
